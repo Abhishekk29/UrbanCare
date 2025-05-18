c199c1361f68bf35412a92783a73d69897ce9656
Abhishekk29 <abhishekanandsharma99@gmail.com>
2025-05-18 21:48:06 +0530

    Footer added,Logo added,minor css updates

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,13 +1,20 @@
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import { useAuth } from '../context/AuthContext';
 import './Navbar.css';
 
 function Navbar() {
   const { token, logout } = useAuth();
-
+  const navigate=useNavigate();
+  const handleclick=()=>{
+    navigate('/');
+  };
   return (
     <nav className="navbar">
-      <h1>Urban Company</h1>
+      <img style={{cursor:'pointer'}}
+      onClick={handleclick}
+      alt="Navbar"
+      src='https://res.cloudinary.com/urbanclap/image/upload/t_high_res_category/w_108,dpr_2,fl_progressive:steep,q_auto:low,f_auto,c_limit/images/growth/home-screen/1687285683825-e6cf23.jpeg'/>
+      {/* <h1>Urban Company</h1> */}
       <div className="links">
         <Link to="/">Home</Link>
         {token ? (
