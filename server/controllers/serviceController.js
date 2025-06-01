ff080a69fe2122fad19dc8acb0a6e874cd5fe328
Abhishekk29 <abhishekanandsharma99@gmail.com>
2025-06-01 21:01:00 +0530

    provider dashboard updated

---
+++ server/controllers/serviceController.js
@@ -0,0 +1,28 @@
+const Service = require('../models/Service');
+
+exports.getAllServices = async (req, res) => {
+  const services = await Service.find().populate('providerId', 'name');
+  res.json(services);
+};
+
+exports.createService = async (req, res) => {
+  const newService = await Service.create({ ...req.body, providerId: req.user.id });
+  res.status(201).json(newService);
+};
+exports.updateService = async (req, res) => {
+  try {
+    const updated = await Service.findByIdAndUpdate(req.params.id, req.body, { new: true });
+    res.json(updated);
+  } catch (err) {
+    res.status(500).json({ message: 'Failed to update service' });
+  }
+};
+
+exports.deleteService = async (req, res) => {
+  try {
+    await Service.findByIdAndDelete(req.params.id);
+    res.json({ message: 'Service deleted' });
+  } catch (err) {
+    res.status(500).json({ message: 'Failed to delete service' });
+  }
+};
